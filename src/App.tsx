2ad73e5b4fdd59c8be37f54aa468338371d3565e
Caio042 <caiohenriquegomeslima@gmail.com>
2022-03-24 23:38:51 -0300

    Define estilos globais

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import GlobalStyles from "./styles/GlobalStyles";
+import DashBoard from "./pages/DashBoard";
+
+const App: React.FC = () => {
+    return (
+        <>
+            <GlobalStyles />
+            <DashBoard />
+        </>
+    )
+}
+
+export default App;
